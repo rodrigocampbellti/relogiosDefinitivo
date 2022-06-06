861b63cd4e9a004e846083f7f10e3407eb351c48
rodrigodoti <103002338+rodrigocampbellti@users.noreply.github.com>
2022-06-06 19:59:31 -0300

    20220606

---
+++ js/funcao.js
@@ -0,0 +1,17 @@
+function mascara_tel(objeto){
+    if(objeto.value.length == 0){
+        objeto.value = objeto.value + '(';
+    }
+    if(objeto.value.length == 3){
+        objeto.value = objeto.value + ')'; 
+    }
+    if(objeto.value.length == 9){
+        objeto.value = objeto.value + '-';
+    }
+}
+
+
+function msg (){
+    alert("Sr(a)" + document.faleconosco.nome.value + ",teus dados foram enviados com sucesso!");
+
+}
